eeb6fd85bfa7b1e0e06f8c7894fef0bcc2038b2d
aribrilliantsyah <ariardiansyah101@gmail.com>
2025-01-14 19:57:03 +0700

    feat(auth): add auth controller and routes setup

--- app/routes/auth.route.go
+++ app/routes/auth.route.go
@@ -1,1 +1,10 @@
 package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
+)
+
+func SetupAuthRoutes(app *fiber.App, DB *gorm.DB) {
+	// authController := controllers.NewAuthController(DB)
+}

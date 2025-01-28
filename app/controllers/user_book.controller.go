57dc8fe5884496f426a627d937dfed157e642de1
aribrilliantsyah <ariardiansyah101@gmail.com>
2025-01-28 22:33:33 +0700

    feat(user_book): add user book controller

--- app/controllers/user_book.controller.go
+++ app/controllers/user_book.controller.go
@@ -1,1 +1,13 @@
 package controllers
+
+import "gorm.io/gorm"
+
+type UserBookController struct {
+	DB *gorm.DB
+}
+
+func NewUserBookController(DB *gorm.DB) *UserBookController {
+	return &UserBookController{
+		DB: DB,
+	}
+}

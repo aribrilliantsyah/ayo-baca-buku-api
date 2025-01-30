66e0cac96d24144fc7c0bf7b171398be662ac931
aribrilliantsyah <ariardiansyah101@gmail.com>
2025-01-30 14:41:21 +0700

    feat(controllers): add reading activity controller

--- app/controllers/reading_activity.controller.go
+++ app/controllers/reading_activity.controller.go
@@ -1,1 +1,13 @@
 package controllers
+
+import "gorm.io/gorm"
+
+type ReadingActivityController struct {
+	DB *gorm.DB
+}
+
+func NewReadingActivityController(DB *gorm.DB) *ReadingActivityController {
+	return &ReadingActivityController{
+		DB: DB,
+	}
+}
